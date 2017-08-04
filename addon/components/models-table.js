e018b86c158c655002d3ed9ef18193fff946ff89
Danijel Kosijer <danijel@fsd.rs>
2017-08-04 15:29:44 +0200

    [MERGE] change slider to slideCounter

--- addon/components/models-table.js
+++ addon/components/models-table.js
@@ -1408,13 +1408,13 @@ export default Component.extend({
         }
     },
 
-    //
-    // sliderCounter(){
-    //     let beforeBoxes = this.get('slideCounter');
-    //     console.log('beforeBoxes', beforeBoxes);
-    //
-    //     this.set('slideCounter', 0)
-    // },
+
+    slideCounter(){
+        let beforeBoxes = this.get('slideCounter');
+        console.log('beforeBoxes', beforeBoxes);
+
+        this.set('slideCounter', 0)
+    },
 
     actions: {
 
