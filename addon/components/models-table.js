2d730d169c134b36910c570dd343c573d23b07f9
Danijel Kosijer <danijel@fsd.rs>
2017-08-16 17:13:19 +0200

    add children ('tr')

--- addon/components/models-table.js
+++ addon/components/models-table.js
@@ -1367,7 +1367,7 @@ export default Component.extend({
         this.set('slideCounter', slideCounter);
         console.log('poziva se funkcija', slideCounter);
         let self = this,
-            myElement = document.getElementsByClassName('hide-mobile-view').childNodes[0];
+            myElement = document.getElementsByClassName('hide-mobile-view').find('tr');
 
         $(myElement).click(function(){
             let slideCounter = self.get('slideCounter');
